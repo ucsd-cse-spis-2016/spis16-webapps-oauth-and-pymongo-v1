f04a9746eccef374e1fdeb0d2a9c5b34c4b09025
Phill Conrad <pconrad@cs.ucsb.edu>
2016-08-28 01:27:35 -0700

    working with org

--- webapp.py
+++ webapp.py
@@ -1,17 +1,23 @@
 from flask import Flask, redirect, url_for, session, request, jsonify
 from flask_oauthlib.client import OAuth
-from flask import render_template, flash
+from flask import render_template, flash, Markup
+
+from github import Github
 
 import pprint
 import os
+import sys
+import traceback
 
 if os.getenv('GITHUB_CLIENT_ID') == None or \
         os.getenv('GITHUB_CLIENT_SECRET') == None or \
-        os.getenv('APP_SECRET_KEY') == None:
+        os.getenv('APP_SECRET_KEY') == None or \
+        os.getenv('GITHUB_ORG') == None:
     raise GithubOAuthVarsNotDefined('''
       Please define environment variables:
          GITHUB_CLIENT_ID
          GITHUB_CLIENT_SECRET
+         GITHUB_ORG
          APP_SECRET_KEY
       ''')
 
@@ -33,7 +39,7 @@ github = oauth.remote_app(
     'github',
     consumer_key=os.environ['GITHUB_CLIENT_ID'],
     consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],
-    request_token_params={'scope': 'user:email'},
+    request_token_params={'scope': 'read:org'},
     base_url='https://api.github.com/',
     request_token_url=None,
     access_token_method='POST',
@@ -44,18 +50,16 @@ github = oauth.remote_app(
 
 @app.context_processor
 def inject_logged_in():
-    print "Checking isLoggedIn"
     return dict(logged_in=('github_token' in session))
+
+@app.context_processor
+def inject_github_org():
+    return dict(github_org=os.getenv('GITHUB_ORG'))
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
-    '''
-    if 'github_token' in session:
-        me = github.get('user')
-        return jsonify(me.data)
-    return redirect(url_for('login'))
-    '''
 
 @app.route('/login')
 def login():
@@ -77,6 +81,7 @@ def logout():
 @app.route('/login/authorized')
 def authorized():
     resp = github.authorized_response()
+
     if resp is None:
         session.clear()
         login_error_message = 'Access denied: reason=%s error=%s full=%s' % (
@@ -85,15 +90,42 @@ def authorized():
             pprint.pformat(request.args)
         )        
         flash(login_error_message, 'error')
+        return redirect(url_for('home'))    
+
+    try:
+        session['github_token'] = (resp['access_token'], '')
+        session['user_data']=github.get('user').data
+        github_userid = session['user_data']['login']
+        org_name = os.getenv('GITHUB_ORG')
+    except Exception as e:
+        session.clear()
+        message = 'Unable to login: ' + str(type(e)) + str(e)
+        flash(message,'error')
+        return redirect(url_for('home'))
+    
+    try:
+        g = Github(resp['access_token'])
+        org = g.get_organization(org_name)
+        named_user = g.get_user(github_userid)
+        isMember = org.has_in_members(named_user)
+    except Exception as e:
+        message = 'Unable to connect to Github with accessToken: ' + resp['access_token'] + " exception info: " + str(type(e)) + str(e)
+        session.clear()
+        flash(message,'error')
+        return redirect(url_for('home'))
+    
+    if not isMember:
+        session.clear() # Must clear session before adding flash message
+        message = 'Unable to login: ' + github_userid + ' is not a member of ' + org_name + \
+          '</p><p><a href="https://github.com/logout" target="_blank">Logout of github as user:  ' + github_userid + \
+          '</a></p>' 
+        flash(Markup(message),'error')
+
     else:
-        try:
-            session['github_token'] = (resp['access_token'], '')
-            session['user_data']=github.get('user').data
-            flash('You were successfully logged in')
-        except:
-            session.clear()
-            flash('Unable to login, please try again',error)
-    return redirect(url_for('home'))
+        flash('You were successfully logged in')
+
+    return redirect(url_for('home'))    
+    
 
 
 @app.route('/page1')
