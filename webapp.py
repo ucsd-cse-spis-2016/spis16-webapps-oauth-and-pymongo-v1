20551856c4b34586154b96726d8fa44e72b68ac7
Phill Conrad <pconrad@cs.ucsb.edu>
2016-08-27 17:40:56 -0700

    try

---
+++ webapp.py
@@ -0,0 +1,77 @@
+from flask import Flask, redirect, url_for, session, request, jsonify
+from flask_oauthlib.client import OAuth
+
+import os
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development'
+oauth = OAuth(app)
+
+# This code originally from https://github.com/lepture/flask-oauthlib/blob/master/example/github.py
+# Edited by P. Conrad for SPIS 2016 to add getting Client Id and Secret from
+# environment variables, so that this will work on Heroku.
+
+class GithubOAuthVarsNotDefined(Exception):
+    '''raise this if the necessary env variables are not defined '''
+
+if os.getenv('GITHUB_CLIENT_ID') == None or \
+        os.getenv('GITHUB_CLIENT_SECRET') == None:
+    raise GithubOAuthVarsNotDefined('''
+      Please define environment variables:
+         GITHUB_CLIENT_ID
+         GITHUB_CLIENT_SECRET
+      ''')
+
+github = oauth.remote_app(
+    'github',
+    consumer_key=os.environ['GITHUB_CLIENT_ID'],
+    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],
+    request_token_params={'scope': 'user:email'},
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize'
+)
+
+
+@app.route('/')
+def index():
+    if 'github_token' in session:
+        me = github.get('user')
+        return jsonify(me.data)
+    return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    return github.authorize(callback=url_for('authorized', _external=True))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('github_token', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/login/authorized')
+def authorized():
+    resp = github.authorized_response()
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error'],
+            request.args['error_description']
+        )
+    session['github_token'] = (resp['access_token'], '')
+    me = github.get('user')
+    return jsonify(me.data)
+
+
+@github.tokengetter
+def get_github_oauth_token():
+    return session.get('github_token')
+
+
+if __name__ == '__main__':
+    app.run()
